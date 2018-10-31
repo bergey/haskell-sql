9d390b1f7fede239b02dae1708d0bba6ace39d99
Daniel Bergey <bergey@teallabs.org>
2018-10-31 18:41:07 -0400

    comment about quasiquotes

--- opaleye-example/src/Main.hs
+++ opaleye-example/src/Main.hs
@@ -42,6 +42,15 @@ personTable = table "personTable" (
         , height = tableColumn "height_inches"
         })
 
+-- What if we could make all the above from a single quasiquote?
+-- The syntax below is exactly like `persistent`; just needs a pile of TH to drive it
+-- [opaleye|
+--     Person sql=personTable
+--         name Text
+--         age Int32
+--         height Double sql=height_inches
+-- |]
+
 personQuery :: Query PersonColumn
 personQuery = queryTable personTable
 

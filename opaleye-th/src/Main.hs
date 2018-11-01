10276e97ffe8fb08d8baddf50e6d85119ec3ac33
Daniel Bergey <bergey@teallabs.org>
2018-10-31 23:12:50 -0400

    macros to generate the parameterized type & type aliases

--- opaleye-th/src/Main.hs
+++ opaleye-th/src/Main.hs
@@ -6,9 +6,9 @@
 
 module Main where
 
-import TH
+import           TH
 
--- import           Data.Profunctor.Product (p2, p3)
+import           Data.Profunctor.Product (ProductProfunctor, (****), (***$))
 -- import           Data.Profunctor.Product.Default (Default)
 import           Data.Profunctor.Product.TH (makeAdaptorAndInstance)
 import           Opaleye
@@ -21,42 +21,26 @@ import           Database.PostgreSQL.Simple hiding (Query)
 import qualified Data.Aeson as JSON
 import qualified Data.Vector as V
 
--- personTable :: Table (Column SqlText, Column SqlInt4, Column SqlFloat8)
---                      (Column SqlText, Column SqlInt4, Column SqlFloat8)
--- personTable = table "personTable" (p3 ( tableColumn "name"
---                                       , tableColumn "age"
---                                       , tableColumn "height" ))
-
-personMeta :: TableMeta
-personMeta = TableMeta
-    { haskellName = "Person"
-    , sqlName = "people"
-    , columns =
-            [ ColumnMeta "name" "name" ''Text
-            , ColumnMeta "age" "age" ''Int
-            , ColumnMeta "height" "height_inches" ''Double
-            ]
-    }
-
-data Person' a b c = Person'
-    { name :: a
-    , age :: b
-    , height :: c
-    }
-type Person = Person' Text Int Double
-type PersonColumn = Person' (Column SqlText) (Column SqlInt4) (Column SqlFloat8)
+$(makeTable personMeta)
+-- data Person' a b c = Person'
+--     { name :: a
+--     , age :: b
+--     , height :: c
+--     }
+-- type Person = Person' Text Int Double
+-- type PersonColumn = Person' (Column SqlText) (Column SqlInt4) (Column SqlFloat8)
 $(makeAdaptorAndInstance "pPerson" ''Person')
 
-personTable :: Table PersonColumn PersonColumn
-personTable = table "personTable" (
-    pPerson Person'
-        { name = tableColumn "name"
-        , age = tableColumn "age"
-        , height = tableColumn "height_inches"
-        })
+-- personTable :: Table PersonColumn PersonColumn
+-- personTable = table "personTable" (
+--     pPerson Person'
+--         { name = tableColumn "name"
+--         , age = tableColumn "age"
+--         , height = tableColumn "height_inches"
+--         })
 
-personQuery :: Query PersonColumn
-personQuery = queryTable personTable
+-- personQuery :: Query PersonColumn
+-- personQuery = queryTable personTable
 
 main :: IO ()
 main = do
--- opaleye-th/src/TH.hs
+++ opaleye-th/src/TH.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
 -- | What if we could make all the boilerplate from a single quasiquote?
 -- The syntax below is exactly like `persistent`; just needs a pile of TH to drive it
 -- [opaleye|
@@ -9,17 +12,77 @@
 
 module TH where
 
-import Data.Text (Text)
 import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+import Data.Traversable
+
+import Opaleye (Column, SqlText, SqlInt4, SqlFloat8)
+import           Data.Profunctor.Product.TH (makeAdaptorAndInstance)
 
 data ColumnMeta = ColumnMeta
-    { haskellName :: Text
-    , sqlName :: Text
-    , haskellType :: Name
+    { cmHaskellName :: String
+    , cmSqlName :: String
+    , cmHaskellType :: TypeQ
+    , cmSqlType :: TypeQ
     }
 
 data TableMeta = TableMeta
-    { haskellName :: Text
-    , sqlName :: Text
+    { haskellName :: String
+    , sqlName :: String
     , columns :: [ColumnMeta]
+    }
+
+a1Names :: Int -> [String]
+a1Names n = take n names where
+  names = [c : "" | c <- ['a'..'z']] ++ [ c : show i | i <- [1..], c <- ['a'..'z'] ]
+
+recordConstructor :: String -> Name -> VarBangType
+recordConstructor field tyN =
+    (mkName field, bang, VarT tyN)
+    where bang = Bang NoSourceUnpackedness NoSourceStrictness
+
+makePrimedType :: Name -> [ColumnMeta] -> Q Dec
+makePrimedType name columns = do
+    tyVars <- traverse newName (a1Names (length columns))
+    let con = RecC name (zipWith recordConstructor (map cmHaskellName columns) tyVars)
+    return $ DataD [] name (map PlainTV tyVars) Nothing [con] []
+
+-- makeHaskellAlias :: String -> Name -> [ColumnMeta] -> Q Dec
+-- makeHaskellAlias name primedName columns = do
+--     tyVars <- for columns $ \(ColumnMeta _ _ ty)  -> do
+--         (TyConI tyCon) <- reify ty
+--         case tyCon of
+--             DataD _ nm [] cs _ _  -> return (nm, tyVars, cs)
+--             NewtypeD _ nm [] c _ -> return (nm, tyVars, [c])
+--             TySynD _ [] ty -> return ty
+--             _ -> fail "tyCon must be a Type of kind Type"
+--     tySynD (mkName name) [] (foldl apply (conT primedName) tyVars)
+
+makeHaskellAlias :: String -> Name -> [ColumnMeta] -> Q Dec
+makeHaskellAlias name primedName columns =
+    tySynD (mkName name) [] (foldl appT (conT primedName) (map cmHaskellType columns))
+
+type family SqlType t
+type instance SqlType String = SqlText
+type instance SqlType Int = SqlInt4
+type instance SqlType Double = SqlFloat8
+
+makeTable :: TableMeta -> Q [Dec]
+makeTable t = do
+    let primedName = mkName (haskellName t ++ "'")
+    primedType <- makePrimedType primedName (columns t)
+    haskellTypeAlias <- makeHaskellAlias (haskellName t) primedName (columns t)
+    sqlAlias <- tySynD (mkName (haskellName t ++ "Column")) [] (foldl appT (conT primedName) (map cmSqlType (columns t)))
+    -- adaptor <- makeAdaptorAndInstance ('p' : haskellName t) primedName
+    return $ [ primedType, haskellTypeAlias, sqlAlias] -- ++ adaptor
+
+personMeta :: TableMeta
+personMeta = TableMeta
+    { haskellName = "Person"
+    , sqlName = "people"
+    , columns =
+            [ ColumnMeta "name" "name" [t|String|] [t|Column SqlText|]
+            , ColumnMeta "age" "age" [t|Int|] [t|Column SqlInt4|]
+            , ColumnMeta "height" "height_inches" [t|Double|] [t|Column SqlFloat8|]
+            ]
     }

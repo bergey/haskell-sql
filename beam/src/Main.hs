916fcbf4a915a879c0ffb81ce06f87ff763b8e7a
Daniel Bergey <bergey@teallabs.org>
2019-01-10 17:09:12 -0500

    beam: order, count(*), join, enum types

--- beam/src/Main.hs
+++ beam/src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DuplicateRecordFields #-}
@@ -11,18 +12,21 @@
 {-# LANGUAGE StandaloneDeriving #-}
 module Main where
 
+import           Data.Aeson as JSON
 import           Data.Coerce
 import           Data.Maybe (fromJust)
 import           Data.Text (Text)
 import           Data.Time
 import           Data.UUID (UUID)
+import           Data.Vector (Vector)
 import           Database.Beam
 import           Database.Beam.Backend.SQL.SQL92
 import           Database.Beam.Postgres
+import           Database.PostgreSQL.Simple.FromField
 import           Prelude
+import           Text.Read as X (readMaybe)
 
-import           Data.Aeson as JSON
-
+import qualified Data.Vector as V
 import qualified Data.UUID as UUID
 
 mkID :: Coercible UUID.UUID id => String -> id
@@ -69,11 +73,25 @@ instance Table TaskT where
     primaryKey = TaskId . taskId
 instance Beamable (PrimaryKey TaskT)
 
+data MeetingType = Slack | Phone | Face
+    deriving (Eq, Enum, Show, Read)
+
+instance FromField MeetingType where
+    fromField f mbs = do
+        x <- readMaybe <$> fromField f mbs
+        case x of
+            Nothing -> returnError ConversionFailed f "Could not 'read' value for 'MeetingType'"
+            Just x -> pure x
+instance FromBackendRow Postgres MeetingType
+instance HasSqlValueSyntax be String => HasSqlValueSyntax be MeetingType where
+  sqlValueSyntax = autoSqlValueSyntax
+
 data MeetingT f = Meeting
     { meetingId :: Columnar f UUID
     , meetingTime :: Columnar f UTCTime
+    , meetingType :: C f (Maybe MeetingType)
     , meetingDetails :: Columnar f JSON.Value
-    , meetingAttendees :: Columnar f [PersonId]
+    , meetingAttendees :: Columnar f (Vector PersonId)
     } deriving Generic
 
 type Meeting = MeetingT Identity
@@ -97,7 +115,11 @@ data TodoDb f = TodoDB
 instance Database be TodoDb
 
 db :: DatabaseSettings be TodoDb
-db = defaultDbSettings
+db = defaultDbSettings `withDbModification`
+    dbModification {
+        _tasks = modifyTable (const "tasks") $ tableModification
+            { taskOwner = PersonId (fieldNamed "owner") }
+        }
 
 runSql :: Connection -> Pg a -> IO a
 runSql = runBeamPostgresDebug putStrLn
@@ -105,30 +127,91 @@ runSql = runBeamPostgresDebug putStrLn
 main :: IO ()
 main = do
     conn <- connect (ConnectInfo "localhost" 5432 "postgres" "" "")
-    -- runSql conn $
-        -- runInsert $ insert (_people db) $ insertValues
-        --     [ -- Person 3 "Jones" 6 11
-        --      Person 4 "', 1, 2, 3, '" 0 99
-        --     ]
 
-    people <- runSql conn $ runSelectReturningList $ select $
-        all_ (_people db)
+    -- runSql conn $ runInsert $ insert (_people db) $
+    --     insertValues
+    --         [ Person 3 "Jones" 6 11
+    --         , Person 4 "', 1, 2, 3, '" 0 99
+    --         ]
+
+    -- insert rows with default PK
+    runSql conn $ runInsert $ insert (_people db) $
+        insertExpressions
+            [ Person default_ (val_ "Proudhon") (val_ (-1)) (val_ (-1))
+            , Person default_ (val_ "Kropotkin") (val_ 180) (val_ 64)
+            ]
+
+    people <- runSql conn $ runSelectReturningList $ select $ all_ (_people db)
     print @[Person] people
 
+    -- select by ID
     m_marx <- runSql conn $ runSelectReturningOne $ select $
         filter_ ((==. 2) . personId) $
         all_ (_people db)
     print m_marx
 
-    let
-        tasksQ :: Q PgSelectSyntax TodoDb _ (Text, Text)
-        tasksQ  = do
-            person <- all_ (_people db)
-            task <- all_ (_tasks db)
-            guard_ (taskOwner task `references_` person)
-            return (personName person, taskDescription task)
-    dumpSqlSelect $ tasksQ
-    tasks <- runSql conn $ runSelectReturningList $ select $ tasksQ
-    print tasks
+    -- other where clause
+    adults <- runSql conn $ runSelectReturningList $ select $
+        filter_ ((>. 10) . personAge) (all_ (_people db))
+    print adults
 
-    return ()
+    -- order by, limit
+    sorted <- runSql conn $ runSelectReturningList $ select $
+        orderBy_ (\u -> (asc_ (personName u), desc_ (personAge u)))
+        (limit_ 3 (all_ (_people db)))
+    print sorted
+
+    -- aggregate functions - count / min / max - all rows
+    count <- runSql conn $ runSelectReturningList $ select $
+             aggregate_ (\_ -> as_ @Int countAll_) (all_ (_people db))
+    print count
+    eldest <- runSql conn $ runSelectReturningOne $ select $
+             aggregate_ (max_ . personAge) (all_ (_people db))
+    print eldest
+
+    -- aggregate functions - min / max - group by
+    countsByName <- runSql conn $ runSelectReturningList $ select $
+        aggregate_ (\p -> (group_ (personName p), as_ @Int countAll_))
+                          (all_ (_people db))
+    print countsByName
+
+    -- joins
+    assigned <- runSql conn $ runSelectReturningList $ select $ do
+        person <- all_ (_people db)
+        task <- all_ (_tasks db)
+        -- guard_ (primaryKey person ==. taskOwner task)
+        guard_ (taskOwner task `references_` person)
+        return (person, task)
+    print assigned
+
+    assigned2 <- runSql conn $ runSelectReturningList $ select $ do
+        task <- all_ (_tasks db)
+        person <- related_ (_people db) (taskOwner task)
+        return (person, task)
+    print assigned2
+
+    assigned3 <- runSql conn $ runSelectReturningList $ select $ do
+        person <- all_ (_people db)
+        tasks <- oneToMany_ (_tasks db) taskOwner person
+        return (person, tasks)
+    putStr "assigned3"
+    print assigned3
+
+    -- outer join
+    assigned4 <- runSql conn $ runSelectReturningList $ select $
+        outerJoin_ (all_ (_people db)) (all_ (_tasks db)) (\(p, t) -> pk p ==. taskOwner t)
+    putStr "assigned4 " >> print assigned4
+
+    meetings <- runSql conn $ runSelectReturningList $ select $ all_ (_meetings db)
+    putStr "meetings" >> print meetings
+
+    -- let
+    --     tasksQ :: Q PgSelectSyntax TodoDb _ (Text, Text)
+    --     tasksQ  = do
+    --         person <- all_ (_people db)
+    --         task <- all_ (_tasks db)
+    --         guard_ (taskOwner task `references_` person)
+    --         return (personName person, taskDescription task)
+    -- dumpSqlSelect $ tasksQ
+    -- tasks <- runSql conn $ runSelectReturningList $ select $ tasksQ
+    -- print tasks
--- init.sql
+++ init.sql
@@ -22,13 +22,16 @@ INSERT INTO tasks (owner, description) VALUES
     (1, 'feed Marx'),
     (2, 'nap');
 
+CREATE TYPE meeting_type AS enum ('Slack', 'Phone', 'Face');
+
 CREATE TABLE meetings (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     time TIMESTAMPTZ,
+    type meeting_type,
     details JSONB NOT NULL default '{}',
     attendees text[] NOT NULL default '{}'
 );
 
-INSERT INTO meetings (time, details, attendees) VALUES
-    ('2018-01-01', '{"location": "home", "topic": "happy new year"}', '{Daniel, Marx}'),
-    ('2018-10-11', '{"location": "office", "topic": "SQL libraries"}', '{Daniel, Jim}');
+INSERT INTO meetings (time, details, type, attendees) VALUES
+    ('2018-01-01', '{"location": "home", "topic": "happy new year"}', Face, '{Daniel, Marx}'),
+    ('2018-10-11', '{"location": "office", "topic": "SQL libraries"}', Slack, '{Daniel, Jim}');
